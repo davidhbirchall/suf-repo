d6adefc544c723e96d5fcc8f6b92f5f94fb28566
David Birchall <davidbirchall@David-Birchalls-MacBook-Pro.local>
2019-05-12 19:56:56 +0100

    added component js files

--- workout-todo/src/App.js
+++ workout-todo/src/App.js
@@ -2,18 +2,47 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.svg";
+
 class App extends Component {
   render() {
     return (
       <Router>
+
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="https://facebook.com/suitsufitness" target="_blank">
+                <img src={logo} width="30" height="30" alt="SUF" />
+              </a>
+              <Link to="/" className="navbar-brand">SUF Todo App</Link>
+              <div className="collpase navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">To dos</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create Todo</Link>
+                  </li>
+                </ul>
+              </div>
+          </nav>
+
+          <br/>
+
+
+
         {/* Define each View (path) and their Controller (component) */}
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
 
-        <div className="container">
-          <h2>SUF Workout To Do App</h2>
         </div>
+        {/* End Container */}
+
       </Router>
     );
   }
---
+++ workout-todo/src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>To Do List Application</p>
+            </div>
+        )
+    }
+}
+
